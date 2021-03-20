3a1789f57ada7c5c6ff5ac69b729b840e06f476c
labelle <beelsdelonge@gmail.com>
2021-03-20 14:03:35 -0300

    Ajustmens for ingtegration

---
+++ front-end/src/Services/API.js
@@ -0,0 +1,11 @@
+/* import axios from 'axios'
+
+const URL_BASE = 'http://localhost:3001/users'
+
+const create = ({ firstName, lastName, participation }) => 
+  axios
+    .post(`${URL_BASE}`, { firstName, lastName, participation })
+    .then((response) => response.data);
+
+export default { create };
+ */
