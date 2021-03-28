42707f0b7ca7f1e06f1b1c4997bbfba6e6b2564e
labelle <beelsdelonge@gmail.com>
2021-03-28 19:18:23 -0300

    More tests about App.js

--- front-end/src/tests/App.test.js
+++ front-end/src/tests/App.test.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { fireEvent, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import GeneralProvider from '../Context/GeneralProvider';
 import userEvent from '@testing-library/user-event';
-import { createMemoryHistory } from 'history';
 import App from '../App';
 
+// PS: work in progress. I admit is harder than I remember, but I'm not giving up.
 describe('Check the App behavior', () => {
-  // PS: work in progress. I admit is harder than I remember, but I'm not giving up.
 
-  /* it('Check the inputs and the click', () => {
-    const history = createMemoryHistory();
-    const { getByPlaceholderText, getByText, getAllByText } = render(
-      <GeneralProvider history={ history }>
+  it('Check the inputs and the click', () => {
+    const { getByPlaceholderText, getByText } = render(
+      <GeneralProvider>
         <App />
       </GeneralProvider>
     )
@@ -22,13 +20,17 @@ describe('Check the App behavior', () => {
     userEvent.type(firstNameInput, 'Joana');
     userEvent.type(lastNameInput, 'Pimentel');
     userEvent.type(participationInput, 10);
-    // Problem to fireEvent into the button ?
-    fireEvent.click(button);
-    const firstName = getAllByText('Joana');
-    const lastName = getAllByText('Pimentel');
-    const participation = getAllByText('10');
-    expect(firstName).toBeInTheDocument();
-    expect(lastName).toBeInTheDocument();
-    expect(participation).toBeInTheDocument();
-  }) */
+    expect(firstNameInput).toBeInTheDocument();
+    expect(lastNameInput).toBeInTheDocument();
+    expect(participationInput).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+    // Problem to fireEvent the button ?
+    /* fireEvent.click(button);
+    const firstName = getByText('Joana');
+    const lastName = getByText('Pimentel');
+    const participation = getByText('10');
+    expect(firstName.tagName).toBe('TD');
+    expect(lastName.tagName).toBe('TD');
+    expect(participation.tagName).toBe('TD'); */
+  })
 })
